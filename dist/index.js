f26866566f79e9a173bcc60e2114341da5e9dc97
michael <michaelsanchez8815@gmail.com>
2022-01-02 23:30:54 -0600

    finishedpopModal

--- dist/index.js
+++ dist/index.js
@@ -8,11 +8,14 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
-let movieList;
+const root = document.documentElement;
+const body = root.querySelector('body');
+let movieList = [];
 const section = document.getElementById('movie-grid');
 const modalOpen = '[data-open]';
 const modalClose = '[data-close]';
 const isVisible = 'is-visible';
+let openModals = document.querySelectorAll(modalOpen);
 const fetchData = () => __awaiter(void 0, void 0, void 0, function* () {
     const result = yield fetch('https://mcuapi.herokuapp.com/api/v1/movies');
     const json = yield result.json();
@@ -27,7 +30,11 @@ const fetchData = () => __awaiter(void 0, void 0, void 0, function* () {
     }));
     movieList = cardData.filter((movie) => movie.id <= 27);
     paintDom(movieList);
+    openModals = document.querySelectorAll(modalOpen);
+    activateOpenModals();
+    movieCount();
 });
+fetchData();
 const paintCard = (movie, div) => {
     div.innerHTML += `
     <div class="card-container" data-open="${movie.title}" data-type="${movie.phase}">
@@ -83,17 +90,115 @@ const paintDom = (listOfChoice) => {
         }
     }
 };
-const openModal = () => {
-    const openModals = document.querySelectorAll(modalOpen);
+//MovieCount
+const movieCount = () => {
+    const phaseOne = movieList.filter((movie) => movie.phase === 1).length;
+    const phaseTwo = movieList.filter((movie) => movie.phase === 2).length;
+    const phaseThree = movieList.filter((movie) => movie.phase === 3).length;
+    const phaseFour = movieList.filter((movie) => movie.phase === 4).length;
+    const section = document.querySelector('.movie-count');
+    const div = document.createElement('div');
+    div.innerHTML = `
+    <h3 class="header-md">MCU Movie Phase Counts</h3>
+    <div class="count-flex-box">
+    <div class="header-sm">Phase One: ${phaseOne}</div>
+    <div class="header-sm">Phase Two: ${phaseTwo}</div>
+    <div class="header-sm">Phase Three: ${phaseThree}</div>
+    <div class="header-sm">Phase Four: ${phaseFour}</div>
+    </div>
+    `;
+    section.appendChild(div);
+};
+// popup card html
+const popUpCard = (movie) => {
+    const div = document.createElement('div');
+    div.setAttribute('data-animation', 'slideInOutTop');
+    div.setAttribute('id', movie.title);
+    div.className = 'modal';
+    div.innerHTML = `
+    <div class="modal-dialog">
+        <header class="modal-header">
+            <h3>${movie.title}</h3>
+            <i class="fas fa-times" data-close></i>
+        </header>
+
+        <div class="modal-body">
+            <div class="img-wrapper">
+                <img src="${movie.img}"></img>
+            </div>
+            <div class="text-wrapper">
+                <div class="basic-card-info">
+                    <p>Type: ${movie.id}</p>
+                    <p>Level: ${movie.phase}</p>
+                    <p>Attack: ${movie.chronology}</p>
+                    <p>Defense: ${movie.releaseDate}</p>
+                </div>
+                <div class="card-desc">
+                    ${movie.summary}
+                </div>
+            </div>
+            <button id="${movie.title}" class="btn btn-primary pop-btn">Add to Favorites</button>
+        </div>
+
+    </div>
+    `;
+    body.appendChild(div);
+};
+const activateOpenModals = () => {
     for (const ele of openModals) {
         ele.addEventListener('click', function () {
             const modalId = ele.dataset.open;
+            const modalChosen = movieList.filter((movie) => movie.title === modalId);
             if (modalId === 'favorites') {
                 document.getElementById(modalId).classList.add(isVisible);
+                closeModalCheck();
+            }
+            else {
+                console.log(modalChosen[0]);
+                popUpCard(modalChosen[0]);
+                setTimeout(function () {
+                    document.getElementById(modalChosen[0].title).classList.add(isVisible);
+                }, 100);
+                closeModalCheck();
             }
         });
     }
 };
-fetchData();
-openModal(); // I am here
+const closeModalCheck = () => {
+    const closeModal = document.querySelectorAll(modalClose);
+    for (const elm of closeModal) {
+        elm.addEventListener('click', function () {
+            const modalId = this.dataset.close;
+            if (modalId === 'favorites') {
+                document.getElementById(modalId).classList.remove(isVisible);
+            }
+            else {
+                this.parentElement.parentElement.parentElement.classList.remove(isVisible);
+                setTimeout(function () {
+                    elm.parentElement.parentElement.parentElement.remove();
+                }, 400);
+            }
+        });
+    }
+};
+// black screen click escape
+document.addEventListener('click', e => {
+    if (e.target === document.querySelector('.modal.is-visible')) {
+        const module = document.querySelector('.modal.is-visible');
+        module.classList.remove(isVisible);
+        setTimeout(function () {
+            module.remove();
+        }, 400);
+    }
+});
+// ESCAPE KEY
+document.addEventListener('keyup', e => {
+    if (e.key === 'Escape') {
+        const module = document.querySelector('.modal.is-visible');
+        module.classList.remove(isVisible);
+        setTimeout(function () {
+            module.remove();
+        }, 400);
+    }
+});
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,11 @@
-let movieList: MovieData[];
+const root = document.documentElement;
+const body = root.querySelector('body')! as HTMLBodyElement;
+let movieList: MovieData[] = [];
 const section = document.getElementById('movie-grid')! as HTMLElement;
 const modalOpen = '[data-open]';
 const modalClose = '[data-close]';
 const isVisible = 'is-visible';
+let openModals: NodeListOf<HTMLElement> = document.querySelectorAll(modalOpen);
 
 
 interface APIFilteredData {
@@ -39,7 +42,12 @@ const fetchData = async () => {
     }))
     movieList = cardData.filter((movie: MovieData) => movie.id <= 27 );
     paintDom(movieList);
+    openModals = document.querySelectorAll(modalOpen);
+    activateOpenModals();
+    movieCount();
 }
+fetchData();
+
 
 const paintCard = (movie: MovieData, div: HTMLDivElement) => {
     div.innerHTML += `
@@ -58,7 +66,7 @@ const paintCard = (movie: MovieData, div: HTMLDivElement) => {
     `;
 }
 
-const paintPage = (pageOfMovies: MovieData[], firstPage?: string) => {
+const paintPage = (pageOfMovies: MovieData[], firstPage?: string):void => {
     const page = document.createElement('div');
     section.appendChild(page)
     if (firstPage) {
@@ -75,7 +83,7 @@ const paintPage = (pageOfMovies: MovieData[], firstPage?: string) => {
     }
 }
 
-const paintDom = (listOfChoice: MovieData[]) => {
+const paintDom = (listOfChoice: MovieData[]):void => {
     let pageOfMovies: MovieData[] = [];
     for (let index = 0; index < listOfChoice.length; index++) {
         const movie = listOfChoice[index]        
@@ -96,17 +104,117 @@ const paintDom = (listOfChoice: MovieData[]) => {
     }
 }
 
-const openModal = () => {
-    const openModals: NodeListOf<HTMLElement> = document.querySelectorAll(modalOpen);
-    for (const ele of openModals) {
-        ele.addEventListener('click', function() {
-            const modalId = ele.dataset.open;
+//MovieCount
+const movieCount = ():void => {
+    const phaseOne: number = movieList.filter((movie: MovieData) => movie.phase === 1).length;
+    const phaseTwo: number = movieList.filter((movie: MovieData) => movie.phase === 2).length;
+    const phaseThree: number = movieList.filter((movie: MovieData) => movie.phase === 3).length;
+    const phaseFour: number = movieList.filter((movie: MovieData) => movie.phase === 4).length;    
+    const section = document.querySelector('.movie-count')!;
+    const div = document.createElement('div');
+    div.innerHTML = `
+    <h3 class="header-md">MCU Movie Phase Counts</h3>
+    <div class="count-flex-box">
+    <div class="header-sm">Phase One: ${phaseOne}</div>
+    <div class="header-sm">Phase Two: ${phaseTwo}</div>
+    <div class="header-sm">Phase Three: ${phaseThree}</div>
+    <div class="header-sm">Phase Four: ${phaseFour}</div>
+    </div>
+    `;
+    section.appendChild(div);
+}
+
+// popup card html
+const popUpCard = (movie: MovieData):void => {
+    const div = document.createElement('div');
+    div.setAttribute('data-animation', 'slideInOutTop');
+    div.setAttribute('id', movie.title);
+    div.className = 'modal';
+    div.innerHTML = `
+    <div class="modal-dialog">
+        <header class="modal-header">
+            <h3>${movie.title}</h3>
+            <i class="fas fa-times" data-close></i>
+        </header>
+
+        <div class="modal-body">
+            <div class="img-wrapper">
+                <img src="${movie.img}"></img>
+            </div>
+            <div class="text-wrapper">
+                <div class="basic-card-info">
+                    <p>Type: ${movie.id}</p>
+                    <p>Level: ${movie.phase}</p>
+                    <p>Attack: ${movie.chronology}</p>
+                    <p>Defense: ${movie.releaseDate}</p>
+                </div>
+                <div class="card-desc">
+                    ${movie.summary}
+                </div>
+            </div>
+            <button id="${movie.title}" class="btn btn-primary pop-btn">Add to Favorites</button>
+        </div>
+
+    </div>
+    `;
+    body.appendChild(div);
+}
+
+const activateOpenModals = ():void => {
+    for (const ele of openModals) {        
+        ele.addEventListener('click', function() {                
+            const modalId: string | undefined = ele.dataset.open;
+            const modalChosen: MovieData[] | undefined = movieList.filter((movie: MovieData) => movie.title === modalId );        
             if (modalId === 'favorites') {
                 document.getElementById(modalId)!.classList.add(isVisible);
+                closeModalCheck();
+            } else {            
+                console.log(modalChosen[0]);
+                popUpCard(modalChosen[0]);
+                setTimeout(function() {
+                    document.getElementById(modalChosen[0].title)!.classList.add(isVisible);
+                }, 100)
+                closeModalCheck();
             }
         })
     }
 }
 
-fetchData();
-openModal(); // I am here
+const closeModalCheck = ():void => {
+    const closeModal:NodeListOf<HTMLElement> = document.querySelectorAll(modalClose);
+    for (const elm of closeModal) {
+        elm.addEventListener('click', function() {
+            const modalId = this.dataset.close;
+            if (modalId === 'favorites') {
+                document.getElementById(modalId)!.classList.remove(isVisible);
+            } else {
+                this.parentElement!.parentElement!.parentElement!.classList.remove(isVisible);
+                setTimeout(function() {
+                    elm.parentElement!.parentElement!.parentElement!.remove();
+                },400);
+            }
+        })
+    }
+}
+
+// black screen click escape
+document.addEventListener('click', e => {
+    if (e.target === document.querySelector('.modal.is-visible')) {
+        const module = document.querySelector('.modal.is-visible')!;
+        module.classList.remove(isVisible);
+        setTimeout(function() {
+            module.remove();
+        },400);
+    }
+})
+
+// ESCAPE KEY
+document.addEventListener('keyup', e => {
+    if (e.key === 'Escape') {
+        const module = document.querySelector('.modal.is-visible')!;
+        module.classList.remove(isVisible);
+        setTimeout(function() {
+            module.remove();
+        },400);        
+    }
+})
